d8d0658c63add260fd2ed5148557d3ea078dcdf7
carson <you@example.com>
2022-02-25 11:37:45 -0800

    catastrophicforgetting

--- contlearn/gettrainer.py
+++ contlearn/gettrainer.py
@@ -18,7 +18,9 @@ def one_epoch_baseline(model: nn.Module, data_loader: torch.utils.data.DataLoade
     epoch_loss = 0
     optimizer = optim.SGD(params=model.parameters(), lr=lr)
     for input, target in data_loader:
-        input = input.squeeze(1)
+        # no need for the channel dim
+        # bs,1,h,w -> bs,h,w
+        input = input.squeeze(1) 
         input, target = var2device(input), var2device(target)
         optimizer.zero_grad()
         output = model(input)
@@ -37,3 +39,42 @@ def test(model: nn.Module, data_loader: torch.utils.data.DataLoader):
         output = model(input)
         correct += (F.softmax(output, dim=1).max(dim=1)[1] == target).data.sum()
     return correct / len(data_loader.dataset)
+
+def elastic_weight_consolidation_training(
+    model, 
+    epochs, 
+    train_loader,
+    test_loader,
+    test2_loader = None,
+    use_cuda=True, 
+):
+    
+    """
+    This function saves the training curve data consisting
+    training set loss and validation set accuracy over the
+    course of the epochs of training using the 
+    elastic_weight_consolidation method
+    
+    I set this up such that if you provide 2 test sets,you
+    can watch the test accuracy change together during training
+    on train_loder
+    """
+    
+    if torch.cuda.is_available() and use_cuda:
+        model.cuda()
+        
+    train_loss, val_acc, val2_acc = [], [], []
+    
+    for epoch in tqdm(range(epochs)):
+
+        epoch_loss = one_epoch_baseline(model,train_loader)
+        train_loss.append(epoch_loss)
+        
+        acc = test(model,test_loader)
+        val_acc.append(acc.detach().cpu().numpy())
+        
+        if test2_loader is not None:
+            acc2 = test(model,test2_loader)
+            val2_acc.append(acc2.detach().cpu().numpy())
+            
+    return train_loss, val_acc, val2_acc, model 
