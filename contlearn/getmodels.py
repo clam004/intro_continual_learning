3262c30b8102e61221c2377abc9aee807f3787b4
carson <you@example.com>
2022-02-24 19:20:28 -0800

    train

--- contlearn/getmodels.py
+++ contlearn/getmodels.py
@@ -8,14 +8,20 @@ class MLP(nn.Module):
     """
     def __init__(self, hidden_size=400):
         super(MLP, self).__init__()
+        self.flat = Flatten()
         self.fc1 = nn.Linear(28 * 28, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, hidden_size)
         self.fc4 = nn.Linear(hidden_size, 10)
 
     def forward(self, input):
-        x = F.relu(self.fc1(input))
+        x = self.flat(input)
+        x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
         return x
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.shape[0], -1)
--- contlearn/gettrainer.py
+++ contlearn/gettrainer.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.autograd import Variable
+from torch import optim
 import torch.utils.data
 
 
@@ -17,6 +18,8 @@ def one_epoch_baseline(model: nn.Module, data_loader: torch.utils.data.DataLoade
     epoch_loss = 0
     optimizer = optim.SGD(params=model.parameters(), lr=lr)
     for input, target in data_loader:
+        input = input.squeeze(1)
+        print(input.shape)
         input, target = var2device(input), var2device(target)
         optimizer.zero_grad()
         output = model(input)
@@ -25,3 +28,13 @@ def one_epoch_baseline(model: nn.Module, data_loader: torch.utils.data.DataLoade
         loss.backward()
         optimizer.step()
     return epoch_loss / len(data_loader)
+
+def test(model: nn.Module, data_loader: torch.utils.data.DataLoader):
+    model.eval()
+    correct = 0
+    for input, target in data_loader:
+        input, target = variable(input), variable(target)
+        input = input.squeeze(1)
+        output = model(input)
+        correct += (F.softmax(output, dim=1).max(dim=1)[1] == target).data.sum()
+    return correct / len(data_loader.dataset)
